e40b0d0690b59138f354633025024bcb3b596702
Stan Low <a22301613@yahoo.com.tw>
2014-07-25 18:07:44 +0800

    improved comment

--- rulers/lib/rulers/routing.rb
+++ rulers/lib/rulers/routing.rb
@@ -4,8 +4,8 @@ module Rulers
       # 拆網址
       _, cont, action, after = 
         env["PATH_INFO"].split('/',4) # 把env(也就是url)拆成四個部分，這邊取第二節（class）、第三節（action）
-      cont = cont.capitalize # "People"
-      cont += "Controller" # "PeopleController"
+      cont = cont.capitalize # "Quotes"
+      cont += "Controller" # "QuotesController"
 
       # 回傳網址中間的class跟action
       [Object.const_get(cont), action] # const_get是把字串轉為class
