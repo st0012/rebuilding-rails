0e6692c3c1181b3212cfb3286d57550ccddf3430
Stan Low <a22301613@yahoo.com.tw>
2014-07-25 18:01:27 +0800

    chapter-2 done

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,5 +1,10 @@
 class QuotesController < Rulers::Controller
+
   def a_quote
-    "Hello" + "World"
+    "Welcome to " + "\n<pre>\n#{env}</pre>" + "\n<pre>\n#{env["PATH_INFO"]}</pre>"
+  end
+
+  def exceptions
+    raise "It's a bad one!" 
   end
 end
--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -5,10 +5,15 @@ require "rulers/routing"
 module Rulers
   class Application
     def call(env)
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, {"Content-Type" => "text/html"}, []]
+      elsif env["PATH_INFO"] == "/"
+        return [200, {"Content-Type" => "text/html"}, ["Welcome!"]]
+      end
       klass, act = get_controller_and_action(env) # klass為url中的第二節（class）、act就是第三節（action）
       controller = klass.new(env) # 把klass變成一個controller
       text = controller.send(act) # controller執行send的動作把我們在controller中定義的action產生的內容傳送出去
-      [200, {'Content-Type' => 'text/html'},[text]]
+      [200, {"Content-Type" => "text/html"},[text]]
     end
   end
 
